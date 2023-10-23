07b907f52fce19990e2f1a95cb9330981ad81d9b
mskogli <mathsko@stud.ntnu.no>
2023-10-23 16:08:12 +0200

    Add kl loss

--- gmm.py
+++ gmm.py
@@ -1,27 +1,32 @@
 import torch
 
-from torch.distributions import Normal, Categorical, MixtureSameFamily, Independent
+from torch.distributions import (
+    Normal,
+    Categorical,
+    MixtureSameFamily,
+    Independent,
+)
 
 
 def gaussian_ll_loss(
     targets: torch.tensor, logpi: torch.tensor, mu: torch.tensor, sigma: torch.tensor
 ) -> float:
     """
-    Computes the negative log likelihood of the targets under the predicted distribution of the latent space:
+    Calculates the negative log likelihood of the targets under the predicted distribution of the latent space:
     NLL = -logsumexp_k(log(pi_k) - 1/2(y - u)^T*Sigma^-1*(y - u) - 1/2log(det(Sigma)))
 
         Parameters:
-            targets (torch.tensor): latent targets (batch, seq_l, latent_dim)
-            logpi (torch.tensor): log mixing coeffs (batch, seq_l, n_gaussians)
-            mu (torch.tensor): predicted means (batch, seq_l, n_gaussians, latent_dim)
-            sigma (torch.tensor): predicted standard deviations (batch, seq_l, n_gaussians, latent_dim)
+            targets (batch, seq_l, latent_dim): latent targets
+            logpi (batch, seq_l, n_gaussians): log mixing coeffs
+            mu (batch, seq_l, n_gaussians, latent_dim): predicted means
+            sigma (batch, seq_l, n_gaussians, latent_dim): predicted standard deviations
     """
     z_score = (
         targets.unsqueeze(
             2
         )  # (batch, seq_l, latent_dim) -> (batch, seq_l, 1, latent_dim)
-        - mu  # (batch, seq_l, num_gaussians, latent_dim)
-    ) / sigma  # (batch, seq_l, num_gaussians, latent_dim)
+        - mu  
+    ) / sigma
 
     normal_loglik = -1 / 2 * torch.einsum(
         "bsdc, bsdc ->bsd", z_score, z_score
@@ -29,24 +34,29 @@ def gaussian_ll_loss(
 
     loglik = torch.logsumexp(
         logpi + normal_loglik, dim=2
-    )  # GMM LL normalized by sequence length
-
+    ) 
     loglik = loglik.mean()
 
     return -loglik
 
 
 def KL_divergence_loss(
-    logpi: torch.tensor, mu: torch.tensor, sigma: torch.tensor
+    mu: torch.tensor, sigma: torch.tensor
 ) -> float:
     """
-    Calculates the KL-divergence between p and q where p is an arbitrary gaussian distribution and q is the a gaussian with zero mean and unit variance
-    KL = 1/2*sum_1_J(1 + log((sigma)^2) - mu^2 + sigma^2)
+    Calculates the KL divergence (DKL) across a normalized across n_gaussians, seq_l and batch
+
+    -DKL = 1/2*sum_1_J(1 + log((sigma)^2) - mu^2 - sigma^2)
 
     see https://arxiv.org/pdf/1312.6114.pdf - Appendix B
-    """
 
-    pass
+        Parameters:
+            mu (batch, seq_l, n_gaussians, latent_dim): means
+            sigma (batch, seq_l, n_gaussians, latent_dim): predicted standard deviations
+    """
+    kl_div_loss = 1/2*torch.sum(torch.ones_like(sigma) + torch.log(torch.square(sigma)) - torch.square(mu) - torch.square(sigma), -1)
+    kl_div_loss = kl_div_loss.mean()
+    return -kl_div_loss
 
 
 def sample_gmm(pi: torch.tensor, mu: torch.tensor, sigma: torch.tensor) -> torch.tensor:
@@ -54,16 +64,12 @@ def sample_gmm(pi: torch.tensor, mu: torch.tensor, sigma: torch.tensor) -> torch
     Sample from a gaussian mixture model
 
         Parameters:
-            pi (n_gaussians)
-            mu (n_gaussians, gaussian_dim)
-            sigma (n_gaussians, gaussian_dim)
+            pi (n_gaussians): mixing coefficents
+            mu (n_gaussians, gaussian_dim): means
+            sigma (n_gaussians, gaussian_dim): standard deviations
     """
-
     categorical = Categorical(probs=pi)
-    # gaussians = [Normal(loc=mu[i], scale=sigma[i]) for i in range(pi.shape[0])]
-
     gaussians = Independent(Normal(loc=mu, scale=sigma), 1)
-
     mixture_dist = MixtureSameFamily(categorical, gaussians)
 
     sample = mixture_dist.sample()
